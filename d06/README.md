171a81fff9a8639205cd1d62cd14e37f8a15718e
Michael E Uhl <michael.uhl@gmail.com>
2025-12-06 09:44:44 -0500

    Day 6 - Part 2
    * Works, but seems overly complicated.  May try to simplify

--- d06/README.md
+++ d06/README.md
@@ -8,18 +8,25 @@ The text of the problem is available [here](https://adventofcode.com/2025/day/6)
 
 #### What are we asked to find?
 
+* The sum of the answers to the individual homework problems
 
 #### What do we know?
 
+* We're given a set of math problems
+* The problems are laid out in columnar format with the operator at the bottom of a set of inputs
 
 #### What can we assume?
 
+* Looking at the input, it looks like the number of inputs for each operator is the same
 
 ### Explore
 
 
 ### Plan
 
+* Read each line of the input, use split() to break into individual input
+* For numerical inputs, put each row into an operands 
+* For operators, put them into a separate list
 
 ## Part 2
 
@@ -29,9 +36,12 @@ The text of the problem is available [here](https://adventofcode.com/2025/day/6#
 
 #### What are we asked to find?
 
+Same as before, but the interpretation of the data is now different
 
 ### Explore
 
 
 ### Plan
 
+* Read each line as a string
+* When we get to the operators, we'll use the operator positions to split the data rows because the operator is always in the first column of the applicable data set
--- d06/py/main.py
+++ d06/py/main.py
@@ -25,6 +25,43 @@ def main(options):
 
     print(f"Total {tot}")
 
+    data_lines: list[str] = []
+    operators: list[Callable] = []
+    splits: list[int] = []
+
+    with open(options.DATA, "rt") as input_file:
+        for line in input_file:
+            if "+" not in line:
+                data_lines.append(line.strip("\n"))
+            else:
+                for i, c in enumerate(line.strip("\n")):
+                    if c != " ":
+                        splits.append(i)
+                        operators.append(OP_MAP[c])
+                splits.append(len(line))
+    # Calculate the field widths
+    splits = [e - s for s, e in zip(splits[:-1], splits[1:])]
+
+    iblocks: list[list[str]] = []
+
+    for d_line in data_lines:
+        row: list[str] = []
+        for l in splits:
+            row.append(d_line[:l])
+            d_line = d_line[l:]
+        iblocks.append(row)
+
+    tot = 0
+    for *chunk, op in zip(*iblocks, operators):
+        args: list[int] = []
+        for d_chars in zip(*chunk):
+            if all([c == " " for c in d_chars]):
+                continue
+            args.append(int("".join(d_chars).strip()))
+        tot += reduce(op, args)
+
+    print(f"Total: {tot}")
+
 
 if __name__ == "__main__":
     from argparse import ArgumentParser
