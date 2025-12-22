2af5ba7621e02e29c4299ca19f64e8f1b808c69c
Michael E Uhl <michael.uhl@gmail.com>
2025-12-22 16:01:04 -0500

    Day 11, Part 2

--- d11/README.md
+++ d11/README.md
@@ -34,9 +34,16 @@ The text of the problem is available [here](https://adventofcode.com/2025/day/11
 
 #### What are we asked to find?
 
+* The number of paths from svr->{fft&dac}->out
 
 ### Explore
 
 
 ### Plan
+
+* Tried a bunch of different things, but made a caching tracer, with arbitrary start/stop nodes
+* Then searched for the paths to the end starting at dac and fft.  fft took too long, so I assumed that all paths went from dac->out (seems like a reasonable assumption since part 1 didn't have loops, and you can't have arbitrary ordering without the potential for loops)
+* Then searched for paths from fft->dac
+* Then searched for paths from svr->fft
+* Then just took the product of the results
 
--- d11/py/main.py
+++ d11/py/main.py
@@ -1,16 +1,15 @@
-def trace(nodes: dict[str, list[str]], node: str, hist: list[str]) -> list[list[str]]:
-    hist = [*hist, node]
+def ftrace(nodes: dict[str, list[str]], start: str, end: str, cache: dict):
+    if start == end:
+        return [[end]]
     paths = []
-    for con in nodes[node]:
-        if con not in hist:
-            paths.extend(trace(nodes, con, hist))
-    return (
-        paths
-        if paths
-        else [
-            hist,
-        ]
-    )
+    for con in nodes[start]:
+        con_p = (
+            cache[con]
+            if con in cache
+            else cache.setdefault(con, ftrace(nodes, con, end, cache))
+        )
+        paths.extend([[start, *p] for p in con_p])
+    return paths
 
 
 def main(options):
@@ -21,7 +20,7 @@ def main(options):
             cons = list(con_lbls.strip().split())
             graph[lbl] = cons
 
-    paths = trace(graph, options.start, [])
+    paths = ftrace(graph, options.start, options.out, {})
     print(f"Part 1: {len(paths)}")
 
 
@@ -31,5 +30,6 @@ if __name__ == "__main__":
     parser = ArgumentParser(description="AoC 2025 Day 11")
     parser.add_argument("DATA", type=str, help="Path to data file")
     parser.add_argument("-s", "--start", type=str, default="you", help="Starting Node")
+    parser.add_argument("-o", "--out", type=str, default="out", help="Ending Node")
 
     main(parser.parse_args())
